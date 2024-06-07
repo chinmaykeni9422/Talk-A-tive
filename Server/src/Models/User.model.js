7667646b70af2fec4dc5280aca8fa7bf2f6b38e2
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-06-07 17:18:53 +0530

    user model

---
+++ Server/src/Models/User.model.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    pic: {
+      type: String,
+      require: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", User);
+
+export default User;
