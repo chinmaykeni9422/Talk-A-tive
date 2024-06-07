ad26b9a73866beaa0c132fe871e2c985647fe2c4
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-06-07 10:30:14 +0530

    json files

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {Route, createBrowserRouter, RouterProvider, createRoutesFromElements, Routes} from "react-router-dom" ;
+import Home from "./Components/Home.jsx"
+import Chat from "./Components/Chat.jsx"
+import Layout from './Layout.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="chat" element={<Chat />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
