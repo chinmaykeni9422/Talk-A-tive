87f77080e93e8516112855f7c47aea7a70cefc3c
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-06-07 10:46:09 +0530

    chat file

--- Client/src/Components/Chat.jsx
+++ Client/src/Components/Chat.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import axios from "axios" ;
+import { useEffect } from "react";
 
 function Chat() {
+
+  const Chat = async () => {
+    const {data} = await axios.get("/api/v1/users/chats");
+    console.log(data) ;
+  }
+
+  useEffect(() => {
+    Chat() ;
+  }, [])
+
   return (
-    <div>Chat</div>
+    <>
+      <div>{}</div>
+    </>
   )
 }
 
