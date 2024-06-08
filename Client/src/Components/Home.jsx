2b4ffc23aa3843dc98ed577ebd21d803fea68ffe
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-06-08 13:52:40 +0530

    tab and home component

--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Tab from './Tab';
 
 function Home() {
   return (
@@ -9,26 +9,8 @@ function Home() {
           Talk-A-Tive
         </div>
 
-        <div className="h-14 w-[750px] text-center flex justify-center rounded-lg text-3xl items-center bg-white">
-          <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-            <li class="me-2">
-              <a
-                href="#"
-                class="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active"
-                aria-current="page"
-              >
-                Tab 1
-              </a>
-            </li>
-            <li class="me-2">
-              <a
-                href="#"
-                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white"
-              >
-                Tab 2
-              </a>
-            </li>
-          </ul>
+        <div className=" text-center flex justify-center rounded-lg text-3xl items-center bg-white">
+          <Tab />
         </div>
       </div>
     </>
---
+++ Client/src/Components/Tab.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const Tabs = () => {
+
+  const [openTab, setOpenTab] = React.useState(1);
+  
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full p-4">
+          <ul
+            className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row"
+            role="tablist"
+          >
+            <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+              <a
+                className={
+                  "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                  (openTab === 1
+                    ? `text-white bg-blue-600`
+                    : `text-blue-600 bg-white`)
+                }
+                onClick={() => {
+                  setOpenTab(1);
+                }}
+              >
+                LogIn
+              </a>
+            </li>
+
+            <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+              <a
+                className={
+                  "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                  (openTab === 2
+                    ? `text-white bg-blue-600`
+                    : `text-blue-600 bg-white`)
+                }
+                onClick={() => {
+                  setOpenTab(2);
+                }}
+              >
+                SignUp
+              </a>
+            </li>
+          </ul>
+
+          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+            <div className="px-4 py-5 flex-auto">
+              <div className="tab-content tab-space">
+                <div className={openTab === 1 ? "block" : "hidden"}>
+                  <h4>hii</h4>
+                </div>
+
+                <div className={openTab === 2 ? "block" : "hidden"}>
+                  <h4>heyy</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Tabs ;
