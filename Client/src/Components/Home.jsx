a55131b5d7d3cd9c782f3dbd1af1ee14311c5fb9
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-06-08 18:54:42 +0530

    home component

--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -4,12 +4,12 @@ import Tab from './Tab';
 function Home() {
   return (
     <>
-      <div class="flex flex-col items-center  pt-10 h-screen bg-gradient-to-r from-sky-500 to-indigo-500">
-        <div className="h-14 w-[750px] text-center mb-3 flex justify-center rounded-lg text-3xl items-center bg-white">
+      <div class="flex flex-col items-center  pt-5 h-screen bg-gradient-to-r from-sky-500 to-indigo-500">
+        <div className="h-14 w-[600px] text-center mb-1 flex justify-center rounded-lg text-3xl items-center bg-white">
           Talk-A-Tive
         </div>
 
-        <div className=" text-center flex justify-center rounded-lg text-3xl items-center bg-white">
+        <div className="w-[600px] text-center flex justify-center rounded-lg text-3xl items-center bg-white">
           <Tab />
         </div>
       </div>
