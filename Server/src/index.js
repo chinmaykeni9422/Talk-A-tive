e760b094ef113083e3e915e2950a9ceca3c70ac5
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-06-09 11:46:43 +0530

    index file

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,11 +1,19 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import connectDB from "./Db/db.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`app Server is running at PORT : ${process.env.PORT} `);
-});
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`app Server is running at PORT : ${process.env.PORT} `) ;
+    })
+})
+.catch((error) => {
+    console.log("MONGODB Connection Failed !!" , error) ;
+}) ;
+ 
  
