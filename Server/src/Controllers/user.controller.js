610754f0f4f3aa1fd3b0fef886ecf81aaa39371f
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-06-10 18:06:52 +0530

    completed register controller

--- Server/src/Controllers/user.controller.js
+++ Server/src/Controllers/user.controller.js
@@ -1,9 +1,10 @@
 import asyncHandler from "express-async-handler";
 import User from "../Models/User.model.js";
+import {uploadOnCloudinary} from "../Utils/cloudnary.js"
 
-const signUpUser = asyncHandler( async (req, res) => {
+const signUpUser = asyncHandler(async (req, res) => {
   //get user details from client
-  const { Name, Email, password, confirmPassword, image } = req.body;
+  const { Name, Email, password, confirmPassword } = req.body;
 
   //validation - noot empty
   if (!Name || !Email || !password || !confirmPassword) {
@@ -18,10 +19,39 @@ const signUpUser = asyncHandler( async (req, res) => {
   }
 
   // check if user already exits
-  const userExists = await User.findOne({Email}) ;
-  if(userExists){
+  const userExists = await User.findOne({ Email });
+  if (userExists) {
     res.status(409);
     throw new Error("User already exists");
+  }
+
+  // handle image files
+  const imageLocalPath = req.files?.image[0]?.path;
+  // if(!imageLocalPath){
+  //   res.status(400);
+  //   throw new Error("image file is required");
+  // }
+  const image = await uploadOnCloudinary(imageLocalPath);
+
+  // create user object - create entry in db
+  const user = await User.create({
+    Name,
+    Email,
+    password,
+    image : image.url
+  }) ;
+
+  // send response to front end
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      Name: user.Name,
+      Email: user.Email,
+      Image: user.Image,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create the User");
   }
 });
 
