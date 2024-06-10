43aad097acac07f4021ae3493b816d9371341ce7
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-06-10 18:07:25 +0530

    added middleware for multer

--- Server/src/Routes/user.routes.js
+++ Server/src/Routes/user.routes.js
@@ -1,10 +1,19 @@
 import {Router} from "express" ;
 import { signUpUser, logInUser } from "../Controllers/user.controller/js";
+import { upload } from "../Middlewares/multer.middleware.js";
 
 
 const router = Router() ;
 
-router.route("/").post(signUpUser) ;
+router.route("/").post(
+    upload.fields([
+        {
+            name: "image",
+            maxCount: 1
+        }
+    ]) ,
+    signUpUser
+) ;
 
 router.route("/login").post(logInUser) ;
 
