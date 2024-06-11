2eba03a7999095f0b0d649f890a923712c6c7eec
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-06-11 20:30:43 +0530

    model updated

--- Server/src/Models/User.model.js
+++ Server/src/Models/User.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt" ;
 
 const userSchema = new Schema(
   {
@@ -35,6 +36,17 @@ const userSchema = new Schema(
   }
 );
 
-const User = mongoose.model("User", User);
+userSchema.pre("save", async function (next){
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
