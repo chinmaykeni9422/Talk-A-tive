99902e3552772eb5692e669fdcef486ee26f682d
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-06-11 20:30:52 +0530

    clundanary uploader

--- Server/src/Utils/cloudnary.js
+++ Server/src/Utils/cloudnary.js
@@ -16,8 +16,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         }) ;
         // file has been uploaded succesfully
-        console.log("file is uploaded on cloudinary", response.url) ;
-        
+        // console.log("file is uploaded on cloudinary", response.url) ;
+        fs.unlinkSync(localFilePath);
         return response ;
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally save temporary file as the uploaded operation failed
