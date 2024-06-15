edb73e28a6dbc3dbcb60024fbddbdcb5b851c57f
Utkars-codes <utkarshkeni1204@gmail.com>
2024-06-15 11:51:17 +0530

    updated pic url handling

--- Server/src/Controllers/user.controller.js
+++ Server/src/Controllers/user.controller.js
@@ -4,8 +4,8 @@ import {uploadOnCloudinary} from "../Utils/cloudnary.js"
 import generateToken from "../config/GenerateToken.js";
 
 const signUpUser = asyncHandler(async (req, res) => { 
-  console.log(req.body) ;
-  // console.log(req.files)
+  console.log("Request Body:", req.body);
+  console.log("Uploaded File:", req.file);
   //get user details from client
   const { name, email, password, confirmPassword } = req.body;
 
@@ -29,20 +29,25 @@ const signUpUser = asyncHandler(async (req, res) => {
   }
 
   // handle image files
-  const imageLocalPath = req.files?.path;
-  // if(!imageLocalPath){
-  //   res.status(400);
-  //   throw new Error("image file is required");
-  // }
-  const pic = await uploadOnCloudinary(imageLocalPath);
-  console.log(pic.url);
+  // Handle image files
+  let picUrl = null;
+  if (req.file) {
+    const imageLocalPath = req.file.path;
+    try {
+      const pic = await uploadOnCloudinary(imageLocalPath);
+      picUrl = pic.url;
+    } catch (error) {
+      res.status(500);
+      throw new Error("Error uploading image to Cloudinary");
+    }
+  }
 
   // create user object - create entry in db
   const user = await User.create({
     name,
     email,
     password,
-    pic: pic.url,
+    pic: picUrl,
   }); ;
 
   // send response to front end
