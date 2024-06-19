37116c3aeb58ac105e286217a6af4d02ae474821
Utkars-codes <utkarshkeni1204@gmail.com>
2024-06-19 08:49:06 +0530

    multiple chats route added

--- Server/src/Routes/chat.routes.js
+++ Server/src/Routes/chat.routes.js
@@ -1,14 +1,14 @@
 import {Router} from "express" ;
 import protect from "../Middlewares/authMiddleware.js";
-import { accessChat } from "../Controllers/chat.controller.js";
+import { accessChat, fetchChats, createGropuChat, renameGroup, addToGroup, removeFromGroup} from "../Controllers/chat.controller.js";
 
 const router = Router() ;
 
 router.route("/").post(protect, accessChat) ;
 router.route("/").get(protect, fetchChats) ;
-// router.route("/group").post(protect, createGropuChat) ;
-// router.route("/rename").put(protect, renameGroup) ;
-// router.route("/groupremove").put(protect, removeFromGroup) ;
-// router.route("/groupadd").put(protect, addToGroup) ;
+router.route("/group").post(protect, createGropuChat) ;
+router.route("/rename").put(protect, renameGroup) ;
+router.route("/groupadd").put(protect, addToGroup) ;
+router.route("/groupremove").put(protect, removeFromGroup) ;
 
 export default router ;
