509b038ff03d985eeee0f71fe1fd141ee48d97b8
Utkars-codes <utkarshkeni1204@gmail.com>
2024-06-21 17:49:30 +0530

    guest user button minor error fux

--- Client/src/Components/Login.jsx
+++ Client/src/Components/Login.jsx
@@ -28,7 +28,7 @@ function Login() {
       const data = response.data ;
       alert(`${data.name} Login Succesfull`) ;
       console.log(data) ;
-      navigate("/") ;
+      navigate("/Chats") ;
     })
     .catch((error) => {
       if(error.response){
@@ -86,10 +86,10 @@ function Login() {
         </button>
 
         <button
-          type="submit"
+          type="button"
           onClick={() => {
             setEmail("guest@example.com") ;
-            setPassword("123456")
+            setPassword("12345612");
           }}
           className="w-full text-white bg-red-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
         >
