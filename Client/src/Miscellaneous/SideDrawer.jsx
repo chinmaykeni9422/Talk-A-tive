d8a1c742182820d1c0f0c1a92a12bd83276ffa4e
Utkars-codes <utkarshkeni1204@gmail.com>
2024-06-22 21:21:54 +0530

    ,

--- Client/src/Miscellaneous/SideDrawer.jsx
+++ Client/src/Miscellaneous/SideDrawer.jsx
@@ -6,7 +6,7 @@ import { chatState } from "../Context/chatContext";
 const SideDrawer = () => {
 
     const {user} = chatState() ;
-
+ 
     const [search, setSearch] = useState("") ;
     const [searchResult, setSearchResult] = useState([]) ;
     const [loading, setLoading] = useState(false) ;
@@ -14,7 +14,7 @@ const SideDrawer = () => {
 
     return(
         <>
-        <div className="flex justify-between items-center bg-white w-[100%] px-5 pt-5 pb-5 border-4">
+        <div className="flex justify-between items-center bg-white w-[100%] px-5 pt-2 pb-2 border-4">
 
             <div>
                 <button className="bg-blue-100 p-2 rounded-md" title="Search Users To Chat">
@@ -35,8 +35,8 @@ const SideDrawer = () => {
                 </menu>
 
                 <menu>
-                    <button className="flex items-center justify-center bg-blue-100 p-2 rounded-md">
-                        <img class="w-12 h-12 rounded-full mr-2 p-1" name={user.name} src="/docs/images/people/profile-picture-5.jpg" alt="Rounded avatar"/>  
+                    <button className="flex cursor-pointer items-center justify-center p-2 rounded-md">
+                        <img class="w-12 h-12 rounded-full mr-2 p-1" src={`${user.pic ? user.pic : "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"}  `} alt="Rounded avatar"/>  
                         <FontAwesomeIcon icon={faChevronDown} />
                     </button>
                 </menu>
